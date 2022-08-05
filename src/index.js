3034f7ff9a104b65268f26693675e273343e4fe2
Florian Antonescu <florian.antonescu@eframe.ch>
2022-08-05 09:48:39 +0200

    add click handler for the upload button: get the text of the INPUT and update the text of the DIV

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-        const input = document.createElement("input");
-        input.setAttribute("type", "text");
-        document.body.appendChild(input);
+function onUploadClick() {
+  // declare a constant variable to access the input text element with the ID 'simpleText' (declared in index.html)
+  const inputElement = document.getElementById('simpleText');
+
+  // declare a constant variable with the text value of the input
+  // see: https://www.w3schools.com/jsref/prop_text_value.asp
+  const textContent = inputElement.value;
+  console.log('input text: ' + textContent)
+
+  // declare a constant to access the DIV element with the ID 'output' (declared in index.html)
+  const outputElement = document.getElementById('output');
+
+  // update the text content of the DIV
+  // see: https://bobbyhadz.com/blog/javascript-change-text-of-div-element
+  outputElement.textContent = textContent
+}
+
     
