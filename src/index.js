305e9a28f8fb595abc7202259944386eb165bf9a
EdwardAdelin <adefinichiu007@gmail.com>
2022-09-06 15:40:48 +0300

    am pus un al doilea buton si text box

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-function onUploadClick() {
+﻿function onUploadClick() {
   // declare a constant variable to access the input text element with the ID 'simpleText' (declared in index.html)
   const inputElement = document.getElementById('simpleText');
-
+  
   // declare a constant variable with the text value of the input
   // see: https://www.w3schools.com/jsref/prop_text_value.asp
   const textContent = inputElement.value;
+ 
   console.log('input text: ' + textContent)
 
   // declare a constant to access the DIV element with the ID 'output' (declared in index.html)
@@ -12,7 +13,22 @@ function onUploadClick() {
 
   // update the text content of the DIV
   // see: https://bobbyhadz.com/blog/javascript-change-text-of-div-element
-  outputElement.textContent = textContent
+  outputElement.textContent = textContent;
 }
 
-    
+function onUploadClick2(){
+const inputElement2 = document.getElementById('complicatedText');
+const textContent2 = inputElement2.value;
+console.log('input text: ' + textContent2)
+const outputElement2 = document.getElementById('output');
+outputElement2.textContent2 = textContent+textContent2;
+}
+
+
+
+
+
+
+
+
+
