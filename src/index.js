e18d2927ffd597d32530039e09582ccea4b03e3a
EdwardAdelin <adefinichiu007@gmail.com>
2022-09-13 13:55:17 +0300

    Concatenare-inputuri

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 ﻿function onUploadClick() {
-  // declare a constant variable to access the input text element with the ID 'simpleText' (declared in index.html)
+
   const inputElement = document.getElementById('simpleText');
-  
-  // declare a constant variable with the text value of the input
-  // see: https://www.w3schools.com/jsref/prop_text_value.asp
+  const inputElement2 = document.getElementById('simpleText2');
   const textContent = inputElement.value;
- 
-  console.log('input text: ' + textContent)
-
-  // declare a constant to access the DIV element with the ID 'output' (declared in index.html)
+  const textContent2 = inputElement2.value;
+  console.log('input text: ' + textContent + textContent2)
   const outputElement = document.getElementById('output');
-
-  // update the text content of the DIV
-  // see: https://bobbyhadz.com/blog/javascript-change-text-of-div-element
-  outputElement.textContent = textContent;
+  outputElement.textContent = textContent+textContent2;
 }
-
-function onUploadClick2(){
-const inputElement2 = document.getElementById('complicatedText');
-const textContent2 = inputElement2.value;
-console.log('input text: ' + textContent2)
-const outputElement2 = document.getElementById('output');
-outputElement2.textContent2 = textContent+textContent2;
-}
-
-
-
-
-
-
-
-
-
